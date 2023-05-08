8da9b57e29df31829cc1dba5c0e8a91b7e0f4428
NishanthSpShetty <nishanthspshetty@gmail.com>
2023-05-08 15:23:47 +0530

    update to v0.1.1

--- Formula/nimble.rb
+++ Formula/nimble.rb
@@ -1,8 +1,8 @@
 class Nimble < Formula
   desc "NimbleEdge CLI: nimble - interface to NimbleEdge systems"
   homepage "https://www.nimbleedge.ai/"
-  url "https://github.com/NimbleEdge/nimblecli/releases/download/v0.1.0/nimblecli_osx_v0.1.0.zip"
-  sha256 "aba977d41e6b60a3c392f17ca1a850cf0344439f5c8f0db1172369b551d45094"
+  url "https://github.com/NimbleEdge/nimblecli/releases/download/v0.1.1/nimblecli_osx_v0.1.1.zip"
+  sha256 "68666f69529e169979af521dc1b7cba03fb38e09bb52b65ae9df4263fcfc2fb5"
   license "BSD-2-Clause"
 
   def install
