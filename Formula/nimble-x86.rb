6be52f8742b10ee743b63296991a0d4bb7b05b09
Kunal Mohan <kunalmohan99@gmail.com>
2023-10-23 18:53:25 +0530

    fix tests

--- Formula/nimble-x86.rb
+++ Formula/nimble-x86.rb
@@ -1,9 +1,9 @@
 class NimbleX86 < Formula
   desc "NimbleEdge CLI: nimble - interface to NimbleEdge systems"
   homepage "https://www.nimbleedge.ai/"
-  license "BSD-2-Clause"
   url "https://github.com/NimbleEdge/nimblecli/releases/download/v0.2.0/nimblecli_osx_v0.2.0.zip"
   sha256 "9b12aa9253af1ff18b94d40f122e84346c8bb0f8452f6310c7db9f28d8f7e03b"
+  license "BSD-2-Clause"
 
   def install
     bin.install "nimble"
@@ -13,4 +13,3 @@ class NimbleX86 < Formula
     system "false"
   end
 end
-
